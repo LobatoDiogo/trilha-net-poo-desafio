6281b9bfdcdf6b9fad8684510421d71634b31e3d
José Diogo Lobato Ribeiro <jdiogolobato@gmail.com>
2023-12-15 16:25:40 -0300

    desafio poo concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone nokia = new Nokia("123456789", "Nokia 3310", "aaaaaa", 16);
+Smartphone iphone = new Iphone("987654321", "Iphone 14", "bbbbbb", 64);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Linkedin");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
